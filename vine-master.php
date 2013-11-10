3a394dbb7744543d5f99ad1be6c18e0abb683a2e
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-10 13:19:11 +0000

    version 4.0.1
    git-svn-id: https://plugins.svn.wordpress.org/vine-master/trunk@801999 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vine-master.php
+++ vine-master.php
@@ -2,7 +2,7 @@
 /**
 Plugin Name: Vine Master
 Plugin URI: http://wordpress.techgasp.com/vine-master/
-Version: 4.0
+Version: 4.0.1
 Author: TechGasp
 Author URI: http://wordpress.techgasp.com
 Text Domain: vine-master

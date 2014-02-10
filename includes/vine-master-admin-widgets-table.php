3643db24c08e23bb362a4193b5a6c57533e2c2ab
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 21:58:07 +0000

    version 4.3
    git-svn-id: https://plugins.svn.wordpress.org/vine-master/tags/4.3@855116 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/vine-master-admin-widgets-table.php
@@ -0,0 +1,44 @@
+<?php
+if(!class_exists('WP_List_Table')){
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+class vine_master_admin_widgets_table extends WP_List_Table {
+	/**
+	 * Display the rows of records in the table
+	 * @return string, echo the markup of the rows
+	 */
+	function display() {
+?>
+<table class="widefat fixed" cellspacing="0">
+	<thead>
+		<tr>
+			<th id="columnname" class="manage-column column-columnname" scope="col" width="300"><legend><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Screenshot', 'vine_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Description', 'vine_master'); ?></h3></legend></th>
+		</tr>
+	</thead>
+
+	<tfoot>
+		<tr>
+			<th class="manage-column column-columnname" scope="col" width="300"><a class="button-primary" href="/wp-admin/widgets.php" title="To Widgets Page" style="float:left;">To Widgets Page</a></p></th>
+			<th class="manage-column column-columnname" scope="col"><a class="button-primary" href="/wp-admin/widgets.php" title="To Widgets Page" style="float:right;">To Widgets Page</a></p></th>
+		</tr>
+	</tfoot>
+
+	<tbody>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-vinemaster-admin-widget-viral.png', __FILE__); ?>" alt="<?php echo get_option('vine_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Vine Advanced Viral Buttons Widget</h3><p>The Advanced Viral Twitter Follow and Tweet buttons will make your website and videos "virulent". Watch those wordpress visits explode!.</p><p>Looks great when published under the player widget, remember you can always hide the widget title to get the button closer to your video.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-vinemaster-admin-widget-player.png', __FILE__); ?>" alt="<?php echo get_option('vine_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Vine Advanced Responsive Player Widget</h3><p>This "Top of the Line" Advanced Responsive Video Player let's you integrate the exciting twitter Vine Videos into any Wordpress widget position. Bombastic!!! Makes <b>NO USE</b> of nasty Javascipt or Ajax.</p><p>Beautiful Adaptive Player in html5 and CSS gives full control over the video appearance, <b>simple with overlay</b> or <b>postcard</b>.</p><p>Check Add-ons Page.</p></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-admin-widget-blank.png', __FILE__); ?>" alt="<?php echo get_option('amazon_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Suggest a Widget</h3><p>Would you like to see your widget idea added to this plugin? Just drop us a line and we will make sure it gets included in the next release.</p><p>Get in touch with TechGasp.</p></td>
+		</tr>
+	</tbody>
+</table>
+<?php
+		}
+}

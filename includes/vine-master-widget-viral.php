476cf3bb27d819a54b4fef68d028913d51f5169c
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-07 23:03:39 +0000

    version 4.4.2.3
    git-svn-id: https://plugins.svn.wordpress.org/vine-master/tags/4.4.2.1@1240225 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/vine-master-widget-viral.php
@@ -0,0 +1,105 @@
+<?php
+//Hook Widget
+add_action( 'widgets_init', 'vine_master_widget_viral' );
+//Register Widget
+function vine_master_widget_viral() {
+register_widget( 'vine_master_widget_viral' );
+}
+
+class vine_master_widget_viral extends WP_Widget {
+	function vine_master_widget_viral() {
+	$widget_ops = array( 'classname' => 'Vine Master Viral Buttons', 'description' => __('Advanced Viral Twitter Follow and Tweet buttons to make your website and videos "virulent". Watch those wordpress visits explode!. ', 'vine_master') );
+	$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'vine_master_widget_viral' );
+	$this->WP_Widget( 'vine_master_widget_viral', __('Vine Master Viral Buttons', 'vine_master'), $widget_ops, $control_ops );
+	}
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+		//Our variables from the widget settings.
+		$vine_title = isset( $instance['vine_title'] ) ? $instance['vine_title'] :false;
+		$vine_title_new = isset( $instance['vine_title_new'] ) ? $instance['vine_title_new'] :false;
+		$vinespacer = "'";
+		$show_vine_twitter_button = isset( $instance['show_vine_twitter_button'] ) ? $instance['show_vine_twitter_button'] :false;
+		$vine_twitter_user = $instance['vine_twitter_user'];
+		echo $before_widget;
+		
+	// Display the widget title
+	if ( $vine_title ){
+		if (empty ($vine_title_new)){
+			if(is_multisite()){
+			$vine_title_new = get_site_option('vine_master_name');
+			}
+			else{
+			$vine_title_new = get_option('vine_master_name');
+			}
+		echo $before_title . $vine_title_new . $after_title;
+		}
+		else{
+		echo $before_title . $vine_title_new . $after_title;
+		}
+	}
+	else{
+	}
+	//Display Vine Twitter Follow Button
+		if ( $show_vine_twitter_button ){
+		echo '<div>' .
+			'<a href="https://twitter.com/'.$vine_twitter_user.'" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow</a>&nbsp;&nbsp;' .
+			'<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'.$vinespacer.'http'.$vinespacer.':'.$vinespacer.'https'.$vinespacer.';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'.$vinespacer.'://platform.twitter.com/widgets.js'.$vinespacer.';fjs.parentNode.insertBefore(js,fjs);}}(document, '.$vinespacer.'script'.$vinespacer.', '.$vinespacer.'twitter-wjs'.$vinespacer.');</script>' .
+			'<a href="https://twitter.com/share" class="twitter-share-button" data-via="'.$vine_twitter_user.'">Tweet</a>' .
+			'<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'.$vinespacer.'http'.$vinespacer.':'.$vinespacer.'https'.$vinespacer.';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'.$vinespacer.'://platform.twitter.com/widgets.js'.$vinespacer.';fjs.parentNode.insertBefore(js,fjs);}}(document, '.$vinespacer.'script'.$vinespacer.', '.$vinespacer.'twitter-wjs'.$vinespacer.');</script>' .
+			'</div>';
+		}
+		else{
+		}
+	echo $after_widget;
+	}
+	//Update the widget
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		//Strip tags from title and name to remove HTML
+		$instance['vine_title'] = strip_tags( $new_instance['vine_title'] );
+		$instance['vine_title_new'] = $new_instance['vine_title_new'];
+		$instance['show_vine_twitter_button'] = $new_instance['show_vine_twitter_button'];
+		$instance['vine_twitter_user'] = $new_instance['vine_twitter_user'];
+		return $instance;
+	}
+	function form( $instance ) {
+	//Set up some default widget settings.
+	$defaults = array( 'vine_title_new' => __('Vine Master', 'vine_master'), 'vine_title' => true, 'vine_title_new' => false, 'show_vine_twitter_button' => false, 'vine_twitter_user' => false, 'show_vine_tweet_button' => false );
+	$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+		<br>
+		<b>Check the buttons to be displayed:</b>
+	<p>
+	<img src="<?php echo plugins_url('images/techgasp-minilogo-16.png', dirname(__FILE__)); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['vine_title'], true ); ?> id="<?php echo $this->get_field_id( 'vine_title' ); ?>" name="<?php echo $this->get_field_name( 'vine_title' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'vine_title' ); ?>"><b><?php _e('Display Widget Title', 'vine_master'); ?></b></label></br>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'vine_title_new' ); ?>"><?php _e('Change Title:', 'vine_master'); ?></label>
+	<br>
+	<input id="<?php echo $this->get_field_id( 'vine_title_new' ); ?>" name="<?php echo $this->get_field_name( 'vine_title_new' ); ?>" value="<?php echo $instance['vine_title_new']; ?>" style="width:auto;" />
+	</p>
+<div style="background: url(<?php echo plugins_url('images/techgasp-hr.png', dirname(__FILE__)); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('images/techgasp-minilogo-16.png', dirname(__FILE__)); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_vine_twitter_button'], true ); ?> id="<?php echo $this->get_field_id( 'show_vine_twitter_button' ); ?>" name="<?php echo $this->get_field_name( 'show_vine_twitter_button' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_vine_twitter_button' ); ?>"><b><?php _e('Show Twitter Buttons', 'vine_master'); ?></b></label></br>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'vine_twitter_user' ); ?>"><?php _e('insert your Twitter Username:', 'vine_master'); ?></label></br>
+	<input id="<?php echo $this->get_field_id( 'vine_twitter_user' ); ?>" name="<?php echo $this->get_field_name( 'vine_twitter_user' ); ?>" value="<?php echo $instance['vine_twitter_user']; ?>" style="width:auto;" />
+	</p>
+<div style="background: url(<?php echo plugins_url('images/techgasp-hr.png', dirname(__FILE__)); ?>) repeat-x; height: 10px"></div>
+		<p>
+		<img src="<?php echo plugins_url('images/techgasp-minilogo-16.png', dirname(__FILE__)); ?>" style="float:left; width:16px; vertical-align:middle;" />
+		&nbsp;
+		<b>Vine Master Website</b>
+		</p>
+		<p><a class="button-secondary" href="http://wordpress.techgasp.com/vine-master/" target="_blank" title="Vine Master Info Page">Info Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/vine-master-documentation/" target="_blank" title="Vine Master Documentation">Documentation</a> <a class="button-primary" href="http://wordpress.techgasp.com/vine-master/" target="_blank" title="Visit Website">Get Add-ons</a></p>
+	<?php
+	}
+ }
+?>
---
+++ vine-master.php
@@ -0,0 +1,73 @@
+<?php
+/**
+Plugin Name: Vine Master
+Plugin URI: http://wordpress.techgasp.com/vine-master/
+Version: 4.4.2.1
+Author: TechGasp
+Author URI: http://wordpress.techgasp.com
+Text Domain: vine-master
+Description: Vine Master for let's you integrate the exciting Vine Videos into any Wordpress widget position, page or post. Only for professional websites.
+License: GPL2 or later
+*/
+/*
+Copyright 2013 TechGasp  (email : info@techgasp.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+if(!class_exists('vine_master')) :
+///////DEFINE VERSION///////
+define( 'VINE_MASTER_VERSION', '4.4.2.1' );
+
+global $vine_master_name;
+$vine_master_name = "Vine Master"; //pretty name
+if( is_multisite() ) {
+update_site_option( 'vine_master_name', $vine_master_name );
+}
+else{
+update_option( 'vine_master_name', $vine_master_name );
+}
+
+class vine_master{
+public static function content_with_quote($content){
+$quote = '<p>' . get_option('tsm_quote') . '</p>';
+	return $content . $quote;
+}
+//SETTINGS LINK IN PLUGIN MANAGER
+public static function vine_master_links( $links, $file ) {
+if ( $file == plugin_basename( dirname(__FILE__).'/vine-master.php' ) ) {
+		if( is_network_admin() ){
+		$techgasp_plugin_url = network_admin_url( 'admin.php?page=vine-master' );
+		}
+		else {
+		$techgasp_plugin_url = admin_url( 'admin.php?page=vine-master' );
+		}
+	$links[] = '<a href="' . $techgasp_plugin_url . '">'.__( 'Settings' ).'</a>';
+	}
+	return $links;
+}
+
+//END CLASS
+}
+add_filter('the_content', array('vine_master', 'content_with_quote'));
+add_filter( 'plugin_action_links', array('vine_master', 'vine_master_links'), 10, 2 );
+endif;
+
+// HOOK ADMIN
+require_once( dirname( __FILE__ ) . '/includes/vine-master-admin.php');
+// HOOK ADMIN ADDONS
+require_once( dirname( __FILE__ ) . '/includes/vine-master-admin-addons.php');
+// HOOK ADMIN WIDGETS
+require_once( dirname( __FILE__ ) . '/includes/vine-master-admin-widgets.php');
+// HOOK WIDGET VIRAL
+require_once( dirname( __FILE__ ) . '/includes/vine-master-widget-viral.php');
